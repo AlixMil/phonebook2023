46c52c92410be2bd33c9cab4d1388daaf8c07f2b
Aleksei <jojmusor@gmail.com>
2022-12-18 15:48:06 +0300

    make basic logic for APP in reducers

--- redux/contactsSlice.ts
+++ redux/contactsSlice.ts
@@ -1,31 +1,59 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { IContact } from './../types/generalTypes';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootStore } from './store';
+import { isArray } from 'util';
 
-export const constactsSlice = createSlice({
+const initialState: IContact[] = [
+	{ firstName: 'Andrey', lastName: 'Feramonov', phoneNumber: '89771337004', description: 'Like bikes', age: 22, tags: ['helper'], id: '1' }
+]
+
+type IChangeAction<T> = {
+	[K in keyof T]: {
+		value: T[K]
+		fieldToChangeName: K
+	}
+}[keyof T] & { index: number }
+
+type IAssignTagAction = {
+	index: number,
+	value: string
+}
+
+type IRemoveTagAction = {
+	index: number
+	value: string
+}
+
+export const contactsSlice = createSlice({
 	name: "contacts",
-	initialState: [
-		{ firstName: 'Andrey', lastName: 'Feramonov', phoneNumber: '89771337004', description: 'Like bikes', age: 22, tags: ['helper'], id: 1 }
-	],
+	initialState: initialState,
 	reducers: {
-		add: (state, action) => {
+		add: (state, action: PayloadAction<IContact>) => {
 			state.push(action.payload)
 		},
-		remove: (state, action) => {
-			state = state.filter(el => el.id !== action.payload.idForRemove)
+		remove: (state, action: PayloadAction<string>) => {
+			return state.filter((el: IContact) => el.id !== action.payload)
 		},
-		change: (state, action) => {
-			state[action.payload.index][action.payload.fieldToChangeName] = action.payload.value
+		change: (state, action: PayloadAction<IChangeAction<IContact>>) => {
+			const { fieldToChangeName, index, value } = action.payload
+			if (state[index]) state[index] = { ...state[index], [fieldToChangeName]: value }
 		},
-		assignTag: (state, action) => {
-			state[action.payload.index].tags.push(action.payload.newTag)
+		assignTag: (state, action: PayloadAction<IAssignTagAction>) => {
+			const { index, value } = action.payload
+			if (state[index]) {
+				state[index] = { ...state[index], tags: state[index].tags?.concat(value) }
+			}
 		},
-		removeTag: (state, action) => {
-			state[action.payload.index].tags = state[action.payload.index].tags.filter(el => el !== action.payload.tagToRemove)
+		removeTag: (state, action: PayloadAction<IRemoveTagAction>) => {
+			const { index, value } = action.payload
+			const changedTags = state[index].tags?.filter((el: string) => el !== value)
+			if (state[index]) state[index] = { ...state[index], tags: changedTags }
 		}
-	}
+	},
 })
 
-export const { add, remove, change, assignTag, removeTag } = constactsSlice.actions
+export const { add, remove, change, assignTag, removeTag } = contactsSlice.actions
 
-export const selectContacts = (state: any) => state.contacts
+export const selectContacts = (state: RootStore) => state.contacts
 
-export default constactsSlice.reducer;
+export default contactsSlice.reducer;
